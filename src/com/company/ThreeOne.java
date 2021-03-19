d9c199bc7f858f51e74d48511f3ab4e3589e78fd
Andrii <a.shyrshov@gmail.com>
2021-03-19 19:28:35 +0200

    Initial commit

---
+++ src/com/company/ThreeOne.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ThreeOne {
+    public static <string> void main (String[] args) {
+        String firstName = "Den";
+        String lastName = "Brown";
+        char a = '-';
+        char b = '=';
+        System.out.println(a + firstName + b + lastName + a);
+
+    }
+}
