8ea90209d60bd054a62d1dbcf44887d173c606bc
Andrii <a.shyrshov@gmail.com>
2021-03-20 16:13:15 +0200

    4

---
+++ src/com/company/ThreeFour.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ThreeFour {
+    public static void main (String[] args) {
+        String a = "The End";
+        String b = "End The";
+        System.out.println("Sting a starts with The: " + a.startsWith("The"));
+        System.out.println("String b starts with THe: " + b.startsWith("The"));
+    }
+}
