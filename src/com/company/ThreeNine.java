7a2188b062af98ac09327e72ba6e60fdf5ed06ab
Andrii <a.shyrshov@gmail.com>
2021-03-21 12:05:50 +0200

    9

---
+++ src/com/company/ThreeNine.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ThreeNine {
+    public static void main (String[] args) {
+        String s = "SDflkdfs23423";
+        if (s.endsWith(".")) {
+            System.out.println("Result string = " + s);
+        } else {
+            System.out.println("Result string = " + s + ".");
+        }
+    }
+}
