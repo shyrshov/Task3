cff3b62fe86ca699d5dde453ec7b9668354b7c35
Andrii <a.shyrshov@gmail.com>
2021-03-21 17:56:29 +0200

    fix

--- src/com/company/ThreeEight.java
+++ src/com/company/ThreeEight.java
@@ -3,16 +3,8 @@ package com.company;
 public class ThreeEight {
     public static void main (String[] args) {
         String a = "Den, John, Will, Kate, Adam, Robin";
-        char[] names = a.toCharArray();
-        for (int i = 0; i < names.length; i++) {
-            if (names[i] != ' ' && names[i]!= ',') {
-                System.out.print(names[i]);
-            } else {
-                if (names[i] == ',') {
-                    System.out.print("\n");
-                }
-            }
+        for (String name : a.split(", ")) {
+            System.out.println(name);
         }
-
     }
 }
